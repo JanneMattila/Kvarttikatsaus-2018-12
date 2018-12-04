7145afbddab6ac316989af0d902fbd44fbfe1ec4
Janne Mattila <janne.mattila@hotmail.fi>
2018-12-04 12:32:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kvarttikatsaus 12.12.2018
+
+Esimerkkisovellus, jota käytettiin 
+"Tekninen katsaus tulevaisuuteen – Microsoft Kvarttikatsaus 12.12.2018"
+tapahtumassa "Azure DevOps: Azure sovellusten julkaisujen automatisointi" demossa.
---
+++ test/unit/Kvarttikatsaus.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using Kvarttikatsaus.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace Kvarttikatsaus.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
